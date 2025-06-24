279fa5fa3c30e69f5861cecdbc858c1995c75a0b
SHADOW_x78 <107577376+Shadow-x78@users.noreply.github.com>
2025-06-24 14:24:43 +0300

    PterodactylContainers | v8.7 | Update Dockerfile Paths

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,18 +7,19 @@ umask 022
 export TZ=${TZ:-UTC}
 cd /home/container || exit 1
 
-if [[ -z "${STARTUP_FILE}" ]]; then
-    echo "ERROR: STARTUP_FILE environment variable is not set"
+if [[ -z "${STARTUP}" ]]; then
+    echo "ERROR: STARTUP environment variable is not set"
     exit 1
 fi
 
-PARSED=$(echo "${STARTUP_FILE}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 PARSED=$(eval echo "$PARSED")
 
 if [[ -z "${PARSED}" ]]; then
-    echo "ERROR: Parsed startup file is empty"
+    echo "ERROR: Parsed startup command is empty"
     exit 1
 fi
 
-echo >&2 "Starting: $PARSED"
+echo ":/home/container\$ ${PARSED}"
+
 ${PARSED}
